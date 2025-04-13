074343fd390c7c12e9ba16fa1a204f0257acf2cc
Breno Zukowski <brenozukotech@gmail.com>
2025-04-13 15:52:38 -0300

    Add get save contacts function

--- apps/api/src/services/whatsapp.ts
+++ apps/api/src/services/whatsapp.ts
@@ -1,9 +1,43 @@
 import { Client, NoAuth } from "whatsapp-web.js";
+import { prisma } from "../lib/db";
 
 let client: Client | null = null;
 let qrCode: string | null = null;
 let connectionState: "loading" | "ready" | "disconnected" | "error" =
   "disconnected";
+let lastQrCode: string | null = null;
+let isAddingContacts = false;
+
+const saveContacts = async (client: Client) => {
+  try {
+    isAddingContacts = true;
+    const contacts = await client.getContacts();
+
+    // Save each contact to the database
+    for (const contact of contacts) {
+      if (contact.number) {
+        await prisma.contact.upsert({
+          where: {
+            phone: contact.number,
+          },
+          update: { name: contact.name || contact.number },
+          create: {
+            name: contact.name || contact.number,
+            phone: contact.number,
+          },
+        });
+      }
+    }
+
+    console.log(
+      `Successfully saved ${contacts.length} contacts to the database`
+    );
+  } catch (error) {
+    console.error("Error saving contacts:", error);
+  } finally {
+    isAddingContacts = false;
+  }
+};
 
 export const initializeWhatsApp = () => {
   if (client) return client;
@@ -17,15 +51,16 @@ export const initializeWhatsApp = () => {
 
   client.on("qr", (qr) => {
     qrCode = qr;
+    lastQrCode = qr;
   });
 
-  client.on("authenticated", () => {
-    connectionState = "loading";
-  });
-
-  client.on("ready", () => {
+  client.on("ready", async () => {
     console.log("Client is ready!");
     connectionState = "ready";
+    qrCode = null;
+    if (client) {
+      await saveContacts(client);
+    }
   });
 
   client.on("disconnected", () => {
@@ -36,6 +71,10 @@ export const initializeWhatsApp = () => {
   client.on("auth_failure", () => {
     console.log("Authentication failed");
     connectionState = "error";
+  });
+
+  client.on("change_state", (state) => {
+    console.log("Client state changed:", state);
   });
 
   client.initialize();
@@ -49,4 +88,8 @@ export const getQrCode = () => {
 
 export const getConnectionState = () => {
   return connectionState;
+};
+
+export const getIsAddingContacts = () => {
+  return isAddingContacts;
 };
